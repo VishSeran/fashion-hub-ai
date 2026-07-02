236aab29e69ca9e143955cb40d8eb317cbc229b9
VishSeran <vishwa.sovis@gmail.com>
2026-07-02 22:01:37 +0530

    image processing-faiss index updated

--- vector_database/faiss_index.py
+++ vector_database/faiss_index.py
@@ -1,10 +1,27 @@
 import faiss
 
+from configuration.logger import get_logger
+
+logger = get_logger("faiss-index")
 class FaissIndex:
     
     def __init__(self, dim=512):
         self.index = faiss.IndexFlatIP(dim)
         
     def add(self, embeddings):
-         
+        self.index.add(embeddings)
         
+    def search(self, query_embedding, k=5):
+        
+        try:
+            distance, indices = self.index.search(query_embedding,k=k)
+            logger.info("Distance and Indices are created")
+            return distance, indices
+        
+        except ValueError as e:
+            logger.error(f"Value error: {e}")
+            raise
+    
+        except Exception as e:
+            logger.error(f"Error in faiss index search for query embeddings: {e}")
+            raise
